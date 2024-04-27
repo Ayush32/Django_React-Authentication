073ef8e9491b2a572706cfcbd483aac2da746cba
Ayush Gupta <721ayush@gmail.com>
2024-04-27 12:31:42 +0530

    serializer setup

--- backend/userAuth/settings.py
+++ backend/userAuth/settings.py
@@ -179,3 +179,14 @@ DJOSER = {
         'user_delete' : 'djoser.serializers.UserDeleteSerializer',
     },
 }
+
+# mailtrap
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_USE_TLS = True
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = 'sname2116@gmail.com'
+DOMAIN = env("DOMAIN")
+SITE_NAME = "Django Authentication"
--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -5,7 +5,7 @@ from djoser.serializers import UserCreateSerializer
 User = get_user_model()
 
 class CreateUserSerializer(UserCreateSerializer):
-    class Meta(UserCreateSerializer):
+    class Meta(UserCreateSerializer.Meta):
         model = User
         fields = ['id','email','first_name','last_name','password']
         
